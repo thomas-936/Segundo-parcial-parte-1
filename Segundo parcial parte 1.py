b8f28b05f1384cb3f451407cdcaa784ef3db071e
tomas <tomas27@gmail.com>
2025-09-08 19:36:32 -0600

    1

--- Segundo parcial parte 1.py
+++ Segundo parcial parte 1.py
@@ -24,6 +24,35 @@ class Candidata:
     def Mostrar_Info(self):
         return f"{self.codigo} - {self.nombre} ({self.institucion}, {self.municipio}) | Puntaje: {self.Puntaje_Final(): .2f}"
 
+class Concurso:
+    def __init__(self , nombre):
+        self.nombre = nombre
+        self.candidatas = []
+
+    def Inscribir_Candidata (self, candidata):
+        for c in self.candidatas:
+            if not c.codigo == candidata.codigo:
+                raise ValueError("Ya existe una candidata inscrita con este codigo. \n Intente de Nuevo")
+            self.candidatas.append(candidata)
+
+
+    def Registrar_Calificacion(self, codigo, cultura, proyeccion, entrevista):
+        for c in self.candidatas:
+            if c.codigo == codigo:
+                c.agregar_calificacion(cultura, proyeccion, entrevista)
+                return
+        raise ValueError("Candidata no encontrada \n Intente de Nuevo")
+
+    def listar_candidatas(self):
+        return  [c.mostrar_info() for c in self.candidatas]
+
+    def ranking(self):
+        lista = self.candidatas[:]
+        return self._quick_sort(lista)
+
+
+
+
 
 
 
